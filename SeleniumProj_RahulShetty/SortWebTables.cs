ec23600f43f9241dbc3aeb0cc84fedc6094d04fa
krish <krishnareddyp884@gmail.com>
2023-08-19 10:17:46 +0530

    new changes

--- SeleniumProj_RahulShetty/SortWebTables.cs
+++ SeleniumProj_RahulShetty/SortWebTables.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using WebDriverManager.DriverConfigs.Impl;
@@ -26,45 +27,55 @@ namespace SeleniumProj_RahulShetty
             driver.Manage().Window.Maximize();
 
             driver.Url = "https://rahulshettyacademy.com/seleniumPractise/#/offers";
-
         }
         [Test]
         public void SortTable()
         {
             ArrayList a = new ArrayList();
-            String aa=null;
             SelectElement dropdown = new SelectElement(driver.FindElement(By.Id("page-menu")));
             dropdown.SelectByText("20");
 
             // Step 1 - Get all Veg names into arrayList A
 
-            IList<IWebElement> veggies = driver.FindElements(By.XPath("//table[@class='table table-bordered']//tbody//td[1]"));
+            //IList<IWebElement> veggies = driver.FindElements(By.XPath("//table[@class='table table-bordered']//tbody//td[1]")); or
+            IList<IWebElement> veggies = driver.FindElements(By.XPath("//tr//td[1]"));
 
             foreach (IWebElement veggie in veggies)
             {
-                //a.Add(veggie.Text);
-                aa = veggie.Text;
+                a.Add(veggie.Text);
             }
+
             TestContext.Progress.WriteLine("Before Sorting");
-            foreach (var element in aa)
+            foreach (var element in a)
             {
                 TestContext.Progress.WriteLine(element);
             }
+
             // Step 2 - Sort this arrayList A
+            TestContext.Progress.WriteLine("\nAfter Sorting");
             a.Sort();
-            TestContext.Progress.WriteLine("After Sorting");
             foreach (String element in a)
             {
                 TestContext.Progress.WriteLine(element);
-            }    
+            }
 
             // Step 3 - Go and click column to sort
 
+            //Css Locator and xpath locator
+            // (th[aria - label *= 'fruit name']) & (//th[contains(@aria-label,'fruit name')])
+            driver.FindElement(By.CssSelector("th[aria-label *= 'fruit name']")).Click();
+            
             // Step 4 - Sort veggies names into arrayList B
+            ArrayList b= new ArrayList();
+            IList<IWebElement> sortedVeggies = driver.FindElements(By.XPath("//tr//td[1]"));
+
+            foreach (IWebElement veggie in sortedVeggies)
+            {
+                b.Add(veggie.Text);
+            }
 
             // arrayList A to B = equal
-
-
+            Assert.AreEqual(a, b);
 
         }
     }
